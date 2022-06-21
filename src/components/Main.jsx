8597692a1bcaaa0871609122b12ce47ed36cc9c9
xlvxn <alvin.fauzi.aulia@gmail.com>
2022-06-21 14:54:52 +0800

    make getNote function, componentDidMount, get and set all notes at NoteList

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,15 +1,38 @@
 import React from "react";
 import Header from "./Header";
 import Content from "./Content";
-
+import NoteList from "./NoteList";
+import { getInitialData } from "../utils";
 
 class MainPage extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            allNotes: [],
+            notes: [],
+            aksi: ''
+        }
+    }
+    
+    getNote = () => {
+        let notes = [...this.state.allNotes]
+        return notes
+    }
+
+    componentDidMount() {
+        const allNotes = getInitialData()
+    
+        this.setState({
+            allNotes
+        })
+    }
+
     render() {
         return (
             <>
                 <Header />
                 <Content>
-                    <NodeList
+                    <NoteList
                         notes = {this.getNote()}
                     />
                 </Content>
