990b911abf324cdd1202894cd557da790f415b1c
xlvxn <alvin.fauzi.aulia@gmail.com>
2022-06-21 14:19:31 +0800

    add NoteList file, doing some check if the notes file is empty or not

---
+++ src/components/NoteList.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import NoteItem from "./NoteItem";
+
+function NoteList({ notes }) {
+    return (
+        <>
+            {
+                (notes.length > 0) ? (
+                    <div className="notes-list">
+                        {
+                            notes.map((note) => (
+                                <NoteItem note={note} />
+                            ))
+                        }
+                    </div>
+                ) : (
+                    <p className="notes-list__empty-message">
+                        Tidak ada catatan
+                    </p>
+                )
+            }
+        </>
+    )
+}
