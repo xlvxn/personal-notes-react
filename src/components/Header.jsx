a289bb0141441f4486cf419f35412021e53c9477
xlvxn <alvin.fauzi.aulia@gmail.com>
2022-06-21 13:51:47 +0800

    add and set header file

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+class Header extends React.Component {
+    render() {
+        return (
+            <div className="note-app__header-wrapper">
+                <div className="note-app__header">
+                    <h1>Personal Notes</h1>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Header;
