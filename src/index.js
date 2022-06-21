19534a22d2318d26cab99ce4fc27afbae63cc306
xlvxn <alvin.fauzi.aulia@gmail.com>
2022-06-21 14:04:26 +0800

    add one file as a mainpage and use a one file as a content section

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import Header from './components/Header';
+import MainPage from './components/Main';
 
 // import style
 import './styles/style.css';
 
 const root = createRoot(document.getElementById('root'));
-root.render(<Header />);
+root.render(<MainPage />);
