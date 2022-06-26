6bf29ad6dbe58081dc67e67bc1f7d6507b81db4f
xlvxn <alvin.fauzi.aulia@gmail.com>
2022-06-26 13:20:37 +0800

    add input text for search

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,6 +6,7 @@ class Header extends React.Component {
             <div className="note-app__header-wrapper">
                 <div className="note-app__header">
                     <h1>Personal Notes</h1>
+                    <input type="text" placeholder="Cari catatan ..." />
                 </div>
             </div>
         )
